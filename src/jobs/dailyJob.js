2b44d38e0c60fc0526d86ee399b2cd0ecbf94c5a
KimWooHyun <ktkwhms3@gmail.com>
2025-04-17 23:37:16 +0900

    Feat: daily job에 아시아/서울 타임존 설정 추가

--- src/jobs/dailyJob.js
+++ src/jobs/dailyJob.js
@@ -8,29 +8,35 @@ function runDailyJob() {
   console.log('💡 Scheduling Daily Job...');
   const username = process.env.TARGET_USERNAME;
 
-  cron.schedule('0 8 * * *', async () => {
-    // cron.schedule('*/2 * * * *', async () => {
-    console.log(`🚀 Running Crawling Job at ${new Date().toISOString()}`);
-    try {
-      await loginAndSaveCookies(); // ✨ 로그인 & 쿠키 저장
-      console.log('🔑 로그인 완료');
-      const links = await getRecentPostLinks(username);
-      if (links.length === 0) {
-        console.log('📭 새 게시물이 없습니다.');
+  cron.schedule(
+    '0 8 * * *',
+    async () => {
+      // cron.schedule('*/2 * * * *', async () => {
+      console.log(`🚀 Running Crawling Job at ${new Date().toISOString()}`);
+      try {
+        await loginAndSaveCookies(); // ✨ 로그인 & 쿠키 저장
+        console.log('🔑 로그인 완료');
+        const links = await getRecentPostLinks(username);
+        if (links.length === 0) {
+          console.log('📭 새 게시물이 없습니다.');
+          return;
+        }
+        const htmlContent = links
+          .map((link) => `<a href="${link}" target="_blank">${link}</a>`)
+          .join('<br>');
+        const subject = `[InstaBot] ${username}의 새 게시물 ${links.length}건`;
+        await sendEmail(subject, htmlContent, process.env.EMAIL_RECEIVER);
+        console.log('📬 Mail sent successfully');
+        await logout(); // ✨ 로그아웃 처리
+      } catch (error) {
+        console.error('Error in daily job:', error);
         return;
       }
-      const htmlContent = links
-        .map((link) => `<a href="${link}" target="_blank">${link}</a>`)
-        .join('<br>');
-      const subject = `[InstaBot] ${username}의 새 게시물 ${links.length}건`;
-      await sendEmail(subject, htmlContent, process.env.EMAIL_RECEIVER);
-      console.log('📬 Mail sent successfully');
-      await logout(); // ✨ 로그아웃 처리
-    } catch (error) {
-      console.error('Error in daily job:', error);
-      return;
+    },
+    {
+      timezone: 'Asia/Seoul',
     }
-  });
+  );
 }
 
 module.exports = { runDailyJob };
