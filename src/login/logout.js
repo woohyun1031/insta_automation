05dbfe7de6e89459fd8a14b6cff534537c77083e
김우현 <whkim@vgen.co.kr>
2025-04-29 15:10:02 +0900

    Feat: 로깅 및 쿠키 파일 처리를 통해 로그아웃 기능 향상

--- src/login/logout.js
+++ src/login/logout.js
@@ -2,9 +2,15 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 
+require('dotenv').config();
+
 puppeteer.use(StealthPlugin());
 
+const COOKIE_FILE = 'cookies.json';
+
+
 async function logout() {
+  console.log('🔑 Instagram 로그아웃 시작');
   const browser = await puppeteer.launch({
     headless: 'new',
     args: [
@@ -14,21 +20,31 @@ async function logout() {
       '--disable-dev-shm-usage'
     ]
   });
+  console.log('🌐 브라우저 시작');
   const page = await browser.newPage();
+
+  console.log('👾 뷰포트 설정');
   await page.setViewport({ width: 1280, height: 800 });
 
-  const cookies = JSON.parse(fs.readFileSync('cookies.json'));
+  const cookies = JSON.parse(fs.readFileSync(COOKIE_FILE));
+
+  if (!cookies || cookies.length === 0) {
+    console.log('⚠️ 쿠키 파일이 비어있거나 존재하지 않습니다. 로그인 후 쿠키를 저장하세요.');
+    await browser.close();
+    return [];
+  }
+
   await page.setCookie(...cookies);
 
-  await page.goto('https://www.instagram.com/', { waitUntil: 'networkidle2' });
   await page.goto('https://www.instagram.com/accounts/logout/', {
     waitUntil: 'networkidle2',
   });
 
   console.log('👋 로그아웃 완료');
 
-  await page.close();
   await browser.close();
+
+  console.log('👋 페이지 종료');
 }
 
 module.exports = { logout };
